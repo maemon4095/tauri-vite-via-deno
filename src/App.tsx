18bb30bfc46a6ebe98c0a77b28efe26d23bd5b83
maemon4095 <maemondecocted@gmail.com>
2023-10-23 00:01:57 +0900

    deno fs issue patch

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
           />
         </a>
       </div>
-      <h1>Vite + React via Deno</h1>
+      <h1>Vite + React with Deno</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,40 @@
-import { defineConfig } from 'vite';
+import { PluginOption, defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),],
+  plugins: [react(), patchWatcher()],
   clearScreen: false,
   server: {
     port: 5173,
     strictPort: true,
     // disable watcher due to a bug cause crash on tauri build
-    watch: null
+    watch: {}
   },
   envPrefix: ["VITE_", "TAURI_"],
 });
+
+/** Due to fs watcher issue, if watching paths contains a path that is ancestor of "src-tauri", deno throws an exception on tauri build.
+ *  To avoid this issue, we must remove paths that is ancestor of "src-tauri".
+ */
+function patchWatcher(): PluginOption {
+  return {
+    name: "patchWatcher",
+    configureServer(server) {
+      const watcher = server.watcher;
+      const originalAdd = watcher.add.bind(watcher);
+
+      const add = (arg: string | string[]) => {
+        const paths: string[] = Array.isArray(arg) ? arg : [arg];
+
+        const filtered = paths.filter(path => path !== ".");
+
+        originalAdd(filtered);
+
+        return watcher;
+      };
+      watcher.add = add.bind(watcher);
+    }
+
+  };
+}
